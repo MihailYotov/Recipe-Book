235c8422a8a4b053ab4003162a021e8dce756482
mhl-yo <myjubox@gmail.com>
2018-02-10 19:15:36 +0200

    Filling recipe list, details, shopping list, edit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'The description', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg'),
+    new Recipe('A test recipe', 'The description', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.recipes)
   }
 
 }
