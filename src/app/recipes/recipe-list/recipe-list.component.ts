449c0c01c75321714c612dc0bf56b731286f3622
mhl-yo <myjubox@gmail.com>
2018-02-13 18:37:50 +0200

    Simple routing, adding fuctionality.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
 
@@ -8,15 +8,19 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'The description', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg'),
-    new Recipe('A test recipe', 'The description', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg')
+    new Recipe('A test recipe2', 'The description2', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.recipes)
+
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
