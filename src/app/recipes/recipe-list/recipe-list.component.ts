88170006847d0c64fedf045c8c58e480415f74aa
mhl-yo <myjubox@gmail.com>
2018-07-11 13:04:55 +0300

    Adding services. Dropdown

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model'
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,19 +9,11 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'The description', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg'),
-    new Recipe('A test recipe2', 'The description2', 'http://la.tasteterminal.com/files/2012/02/oc-restaurant-week-meat.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
+    this.recipes = this.recipeService.getRecipes();
   }
 }
